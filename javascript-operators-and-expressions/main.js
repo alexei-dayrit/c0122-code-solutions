1c3ba066aeb041ef6ffb1fcbcedf79b764acefef
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-01-31 11:50:53 -0800

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 2;
+var height = 2;
+var area = width * height;
+console.log('area value:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 4;
+var payment = 6;
+var change = payment - bill;
+console.log('change value:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 50;
+var midterm = 80;
+var final = 70;
+var grade = (quizzes + midterm + final) / 3;
+console.log('grade value:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Alexei';
+var lastName = 'Dayrit';
+var fullName = firstName + ' ' + lastName;
+console.log('fullName value:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+console.log('isAcidic value:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 40;
+var isSparta = headCount === 300;
+console.log('isSparta value:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('motto value:', motto);
+console.log('typeof motto:', typeof motto);
