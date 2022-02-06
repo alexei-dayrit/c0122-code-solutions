73821d425b0acf8b47b4f33aa338b76bd93831f2
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-05 18:06:48 -0800

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,11 @@
+/* exported getValue */
+
+/*
+  - Create variable which is assigned object at key
+  - Return variable
+*/
+
+function getValue(object, key) {
+  var val = object[key];
+  return val;
+}
