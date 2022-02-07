44230f5c45fc262ff9ea943177979b1179602bac
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-07 15:58:38 -0800

    Completed dom-manipulation exercise.

---
+++ dom-manipulation/main.js
@@ -0,0 +1,21 @@
+var counter = 0;
+
+var $hotButton = document.querySelector('.hot-button');
+var $counter = document.querySelector('.click-count');
+
+$hotButton.addEventListener('click', function () {
+  $counter.textContent = counter++;
+  if (counter < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (counter < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (counter < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (counter < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (counter < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+});
