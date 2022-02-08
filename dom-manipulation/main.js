a15ff519ac19d2f4e040708ff6bff53ff4ae17da
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-08 10:23:56 -0800

    Made changes to main.js. Removed equal signs in condition statements and stored class names into a variable.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -3,19 +3,22 @@ var counter = 0;
 var $hotButton = document.querySelector('.hot-button');
 var $counter = document.querySelector('.click-count');
 
-$hotButton.addEventListener('click', function () {
-  $counter.textContent = 'Clicks: ' + counter++;
-  if (counter <= 4) {
-    $hotButton.className = 'hot-button cold';
-  } else if (counter <= 7) {
-    $hotButton.className = 'hot-button cool';
-  } else if (counter <= 10) {
-    $hotButton.className = 'hot-button tepid';
-  } else if (counter <= 13) {
-    $hotButton.className = 'hot-button warm';
-  } else if (counter <= 16) {
-    $hotButton.className = 'hot-button hot';
+$hotButton.addEventListener('click', function (event) {
+  counter++;
+  var temperature = null;
+  if (counter < 4) {
+    temperature = 'cold';
+  } else if (counter < 7) {
+    temperature = 'cool';
+  } else if (counter < 10) {
+    temperature = 'tepid';
+  } else if (counter < 13) {
+    temperature = 'warm';
+  } else if (counter < 16) {
+    temperature = 'hot';
   } else {
-    $hotButton.className = 'hot-button nuclear';
+    temperature = 'nuclear';
   }
+  $hotButton.className = 'hot-button ' + temperature;
+  $counter.textContent = 'Clicks: ' + counter;
 });
