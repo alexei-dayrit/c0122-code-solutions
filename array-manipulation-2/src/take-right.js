23954fc026c4934ca6a2d0a169edec11ad997f9c
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-10 22:22:41 -0800

    Started array-manipulation-2.

---
+++ array-manipulation-2/src/take-right.js
@@ -0,0 +1,11 @@
+/* exported takeRight */
+
+function takeRight(array, count) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (i >= count) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
