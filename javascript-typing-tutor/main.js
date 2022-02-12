37860f8e4edc115c5e6e522ea7aa3ac8379def68
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-12 15:53:13 -0800

    Completed javascript-typing-tutor challenge.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,28 +1,17 @@
 var $allSpans = document.querySelectorAll('span');
 var currentIndex = 0;
 
+$allSpans[currentIndex].className = 'current-char';
+
 function handleKeydown(event) {
   if (event.key === $allSpans[currentIndex].textContent) {
-    currentIndex.className = 'underline';
-    $allSpans[currentIndex].className = 'green';
+    $allSpans[currentIndex].className = 'correct';
+    $allSpans[currentIndex + 1].className = 'current-char';
     currentIndex++;
   } else {
-    $allSpans[currentIndex].className = 'red';
+    $allSpans[currentIndex].className = 'wrong';
+
   }
 }
 
 document.addEventListener('keydown', handleKeydown);
-
-/*
-  - Need condition that sets previous keys as green
-    - and moves onto next character
-    - makes current character underline
-    - WHITE SPACE property for spaces!!???!
-*/
-
-// REVERSE OF CURRENT FUNCTION
-// // if (event.key !== $allSpans[i].textContent) {
-// $allSpans[i].className = 'red';
-// } else {
-//   $allSpans[i].className = 'green';
-// }
