b0da5ea118d21ea4b595ed03caa8f9ee2cfdc277
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-11 19:14:17 -0800

    Added comments from Q&A.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -7,9 +7,15 @@ if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
 }
 
-function beforeUnloadListener(event) {
+function handleBeforeUnload(event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
 }
 
-window.addEventListener('beforeunload', beforeUnloadListener);
+window.addEventListener('beforeunload', handleBeforeUnload);
+
+/*
+For event listener functions it's best to name it 'handle..event name'
+If not using handle function in the future, it's okay to use an anonymous function
+in the event listener
+*/
