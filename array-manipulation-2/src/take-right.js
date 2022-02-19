19732515661300516a1ea6d68e7b5890f0e5c895
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-19 12:15:03 -0800

    Made changes to take-right.js, drop.js, and take.js.

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,16 +1,22 @@
 /* exported takeRight */
 
 /*
-
+  - Create new array variable
+  - Loop through array, with initialization at array.length - count
+    - If count is greater than array length,
+      - then return array input
+    - Else push array at current index into new array
+  - Return new array
 */
 
 function takeRight(array, count) {
   var newArray = [];
   for (var i = array.length - count; i < array.length; i++) {
-    if (array.length === 0) {
-      return newArray;
+    if (count > array.length) {
+      return array;
+    } else {
+      newArray.push(array[i]);
     }
-    newArray.push(array[i]);
   }
   return newArray;
 }
@@ -22,15 +28,15 @@ function takeRight(array, count) {
   - If current index is greater than or equal to array length MINUS count,
   (this condition makes it so we don't have to start from beginning of array )
     - Then push array at current index into new array var
-  - Return new array variable
----------------
-function takeRight(array, count) {
-  var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i >= array.length - count) {
-      newArray.push(array[i]);
+    - Return new array variable
+    ---------------
+    function takeRight(array, count) {
+      var newArray = [];
+      for (var i = 0; i < array.length; i++) {
+        if (i >= array.length - count) {
+          newArray.push(array[i]);
+        }
+      }
+      return newArray;
     }
-  }
-  return newArray;
-}
 */
