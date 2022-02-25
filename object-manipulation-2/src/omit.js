039001de846e72ea018c2b91d70184027e3f323a
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-24 21:17:05 -0800

    Changed and completed omit.js

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,38 +2,20 @@
 
 /*
   - Create newObj variable
-  - Create storage variable
   - Loop through source object
-    - If current prop is equal to keys array at i,
-      - Then add it to storage
-    - Else new object at current prop is equal to current key
+    - If keys array DOES NOT (!) include current property name,
+      - set newObj to have current prop name and key
   - Return new object
-
-  // Problem is that key array isn't same order as the actual object
-  // So I need to check if current key is found in the ENTIRE object, not at just that
-  spot of the object
 */
 
 function omit(source, keys) {
-  // var newObject = {};
-  // for (var i = 0; i < keys.length; i++) {
-  //   for (var prop in source) {
-  //     if (keys[i] !== source[prop]) {
-  //       newObject[prop] = source[prop];
-  //     }
-  //   }
-  // }
-  // return newObject;
   var newObject = {};
-  var allKeys = Object.keys(source);
   for (var prop in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i] !== allKeys[i]) {
-        newObject[keys[i]] = source[prop];
-        console.log('object:', newObject[keys[i]]);
-        console.log('source[prop]:', source[prop]);
-      }
+    if (!keys.includes(prop)) {
+      newObject[prop] = source[prop];
     }
   }
   return newObject;
 }
+
+// Need to check if current key is found in the ENTIRE object, not just current prop
