5c95fbced5fd9565c3038e004932b2557abcb2f0
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-25 23:47:40 +0000

    Completed is-anagram.js. Still in progress with title-case.js

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -3,38 +3,39 @@
 /*
   - Create var to split firstString by letter
   - Create var to split secondString by letter
-  - Loop through
+  - Loop through split secondString:
+    - If current index at secondString is NOT an empty space, push into new secondArr
+  - Loop through split firstString:
+    - If current index at firstString is an empty space or undefined, continue
+      - Else, push current index at firstString into new firstArr
+  - Create two variables for firstArr and secondArr
+    - Sort and join both vars
+  - If sorted and joined vars equal each other return true, else return false
 
 */
 function isAnagram(firstString, secondString) {
   var splitFirstStr = firstString.split('');
   var splitSecondStr = secondString.split('');
-  console.log('split first string:', splitFirstStr);
+
   var secondArr = [];
   for (var i = 0; i < splitSecondStr.length; i++) {
     if (splitSecondStr[i] !== ' ') {
       secondArr.push(splitSecondStr[i]);
     }
   }
+
   var firstArr = [];
   for (var a = 0; a < splitFirstStr.length; a++) {
     if (splitFirstStr[a] === undefined || splitFirstStr[a] === ' ') {
-      break;
-      // splice???
+      continue;
     } else {
       firstArr.push(splitFirstStr[a]);
     }
   }
-  // WRONG: SCHOOL returns true and DEBIT CARD returns false
-  // BOTH have first array losing second word, in general first array loses second word
-  console.log('first NO SPACE: ', firstArr);
-  console.log('second NO SPACE: ', secondArr);
 
   var noSpaceFirst = firstArr.sort().join('');
   var noSpaceSecond = secondArr.sort().join('');
 
-  // console.log('first SORTED: ', noSpaceFirst);
-  // console.log('second SORTED: ', noSpaceSecond);
   if (noSpaceFirst === noSpaceSecond) {
     return true;
   } else {
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,9 +1,51 @@
 /* exported titleCase */
 
-// Capitalize the first word of the title and of any subtitle.
-// Capitalize all “major” words(nouns, verbs, adjectives, adverbs, and pronouns) in the title, including the second part of hyphenated major words(e.g., Self - Report not Self - report).
-// Capitalize all words of four letters or more.
+/* TASKS:
+  - Capitalize the first word of the title and of any subtitle.
+  - Capitalize all “major” words(nouns, verbs, adjectives, adverbs, and pronouns) in the title, including the second part of hyphenated major words(e.g., Self - Report not Self - report).
+  - Capitalize all words of four letters or more.
+*/
+// something like if current word is less than certian number count??
+
+/*
+ Loop through string,
+ - if there's a space before or after then capitalize next word
+  - unless it's a preposition
+ - If there's a hyphen capitalize next word
+*/
 
 function titleCase(string) {
-
+  // var split = string.split(' ');
+  // console.log('split', split);
+  // var word1 = split[0].toLowerCase();
+  // console.log(word1);
+  // console.log(word1[0].toUpperCase());
+  // for (var i = 0; i < split.length; i++) {
+  // console.log(string[i]);
+  // var lowerCased = split[i].toLowerCase();
+  // console.log('lower', lowerCased);
+  // var upperCased = lowerCased[i][0].toUpperCase() + lowerCased.slice(1);
+  // console.log('upper:', upperCased);
 }
+
+// FIRST ATTEMPT
+// function titleCase(string) {
+//   var splitWordsArr = string.split(' ');
+//   var lowerCasedArr = [];
+//   var upperCasedArr = [];
+//   for (var i = 0; i < splitWordsArr.length; i++) {
+//     var lowerCased = splitWordsArr[i].toLowerCase();
+//     // lowerCasedArr.push(lowerCased);
+//     if (lowerCased.length > 3) {
+//       var capitalized = lowerCased[0].toUpperCase() + lowerCased.slice(1);
+//       upperCasedArr.push(capitalized);
+//     }
+//   }
+// console.log('upperCased array:', upperCasedArr);
+// console.log('lowerCased array:', lowerCasedArr);
+// var joinedLower = lowerCasedArr.join(' ');
+// console.log('joinedLower words:', joinedLower);
+
+// console.log('lowerCased words:', lowerCased);
+// console.log('capitalized words:', capitalized);
+// }
