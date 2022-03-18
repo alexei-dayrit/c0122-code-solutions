633a92c3b03b29d79128c6a7018ae34182dd14b0
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-03-18 22:55:26 +0000

    Completed level 3 of node-fs-readfile

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,34 +1,22 @@
 const fs = require('fs');
 
 let currentArg = 1;
-const test = (err, data) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log('---------');
-  console.log('data:', data);
-};
 
 function concatReadAll() {
   currentArg++;
-  if (currentArg >= process.argv.length) {
-    return;
+  if (currentArg < process.argv.length) {
+    console.log('currentArg:', currentArg);
+    const filename = process.argv[currentArg];
+    console.log('file:', filename);
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log('---------');
+      console.log('data:', data);
+      concatReadAll();
+    });
   }
-  console.log('currentArg:', currentArg);
-  const filename = process.argv[currentArg];
-  console.log('file:', filename);
-  fs.readFile(filename, 'utf8', test);
-  concatReadAll();
 }
 concatReadAll();
-
-// const filename = process.argv[2];
-// fs.readFile(filename, 'utf8', (err, data) => {
-//   if (err) {
-//     console.error(err);
-//     process.exit(1)
-//   }
-//   console.log('---------');
-//   console.log('data:', data);
-// });
