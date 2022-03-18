e0e33136e196a2f66339fb843fa7fe672e8957b3
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-03-18 19:27:27 +0000

    Still in progress

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,18 +1,34 @@
 const fs = require('fs');
 
 let currentArg = 1;
+const test = (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('---------');
+  console.log('data:', data);
+};
 
 function concatReadAll() {
   currentArg++;
   if (currentArg >= process.argv.length) {
     return;
   }
+  console.log('currentArg:', currentArg);
   const filename = process.argv[currentArg];
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log('---------');
-    console.log(data);
-  });
-  // concatReadAll();
+  console.log('file:', filename);
+  fs.readFile(filename, 'utf8', test);
+  concatReadAll();
 }
 concatReadAll();
+
+// const filename = process.argv[2];
+// fs.readFile(filename, 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     process.exit(1)
+//   }
+//   console.log('---------');
+//   console.log('data:', data);
+// });
