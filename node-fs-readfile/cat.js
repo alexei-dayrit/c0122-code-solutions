9c61ee989c915cff6a54e70a07a004415cf43f04
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-03-19 00:07:53 +0000

    Completed level 3. Removed unnecessary console.logs

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -5,16 +5,13 @@ let currentArg = 1;
 function concatReadAll() {
   currentArg++;
   if (currentArg < process.argv.length) {
-    console.log('currentArg:', currentArg);
     const filename = process.argv[currentArg];
-    console.log('file:', filename);
     fs.readFile(filename, 'utf8', (err, data) => {
       if (err) {
         console.error(err);
         process.exit(1);
       }
-      console.log('---------');
-      console.log('data:', data);
+      console.log(data);
       concatReadAll();
     });
   }
