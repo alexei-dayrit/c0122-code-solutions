161d2e6f6e602d9740469543233965487c4c5459
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-03-29 23:39:08 +0000

    Added sample solution for reference.

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -9,3 +9,18 @@ const element = <CustomButton text="Click Me!"/>;
 const rootDiv = document.querySelector('#root');
 
 ReactDOM.render(element, rootDiv);
+
+/* QnA Solution
+
+const CustomButton = props => {
+  return (
+    <button>Click me!</button>
+  );
+};
+
+ReactDOM.render(
+  <CustomButton />,
+  document.querySelector('#root')
+);
+
+*/
