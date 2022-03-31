49f14bb54220c3fdf39ecb7a1d37d4272ffe968e
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-03-31 21:26:04 +0000

    Changed toggle-switch.jsx to have one handler function and updated styling.

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -6,39 +6,48 @@ export default class ToggleSwitch extends React.Component {
     this.state = ({
       isToggledOn: false
     });
-    this.handleSwitchOn = this.handleSwitchOn.bind(this);
-    this.handleSwitchOff = this.handleSwitchOff.bind(this);
+    // this.handleSwitchOn = this.handleSwitchOn.bind(this);
+    // this.handleSwitchOff = this.handleSwitchOff.bind(this);
+    this.handleSwitch = this.handleSwitch.bind(this);
   }
 
-  handleSwitchOn() {
+  handleSwitch() {
     this.setState({
-      isToggledOn: false
+      isToggledOn: !this.state.isToggledOn
     });
   }
 
-  handleSwitchOff() {
-    this.setState({
-      isToggledOn: true
-    });
-  }
+  // handleSwitchOn() {
+  //   this.setState({
+  //     isToggledOn: false
+  //   });
+  // }
+
+  // handleSwitchOff() {
+  //   this.setState({
+  //     isToggledOn: true
+  //   });
+  // }
 
   render() {
     const isToggledOn = this.state.isToggledOn;
     if (!isToggledOn) {
       return (
           <div className="container">
-            <div onClick={this.handleSwitchOff} className="switch-off">
-              <div onClick={this.handleSwitchOff} className="slider-off"></div>
-              <div onClick={this.handleSwitchOff} className="label-off">OFF</div>
+            <div onClick={this.handleSwitch} className="switch-off">
+              <div onClick={this.handleSwitch} className="slider-off"></div>
+              <div className="label-off">OFF</div>
             </div>
           </div>
       );
-    } else if (isToggledOn) {
+    } else {
       return (
-      <div className="container">
-        <div onClick={this.handleSwitchOn} className="switch-on">
-          <div onClick={this.handleSwitchOn} className="slider-on"></div>
-          <div onClick={this.handleSwitchOn} className="label-on">ON</div>
+      <div className="lights-on">
+        <div className="container lights-on">
+          <div onClick={this.handleSwitch} className="switch-on">
+            <div onClick={this.handleSwitch} className="slider-on"></div>
+            <div className="label-on">ON</div>
+          </div>
         </div>
       </div>
       );
