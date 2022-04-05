2c4684e4bf373498ab5405827d2d9a601e331825
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-04-05 22:19:34 +0000

    Changed returning statement in sql var so client doesn't receive hashed password.

--- api-registration/server/index.js
+++ api-registration/server/index.js
@@ -30,7 +30,7 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       const sql = `
         insert into "users" ("username", "hashedPassword")
         values ($1, $2)
-        returning *
+        returning "createdAt", "username", "userId"
       `;
       const params = [username, hashedPassword];
       db.query(sql, params)
